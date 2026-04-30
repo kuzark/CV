04c0fe23f4054b7f8074d7d26f7789241526def9
kuzark <kuzark@yandex.ru>
2026-04-30 12:26:56 +0500

    Update crud_completer.py

--- crud_completer.py
+++ crud_completer.py
@@ -1,61 +1,164 @@
-'''Класс для работы с базой данных'''
+'''Классы для работы с базой данных'''
 
-from tkinter.messagebox import showerror, showinfo, askyesno
-from sqlalchemy.orm import sessionmaker
+from abc import ABC, abstractmethod
+from tkinter.messagebox import showerror, showwarning
 
 # Мои модули
-from app_data import ERROR_MSG, WARNING_MSG, INFO_MSG, NO_PATIENT
-from .models import Patient
-from .log_handler import log_patient_attrs
+from app_data import ERROR_MSG, WARNING_MSG
 
-class CRUDcompleter:
-    '''Выполняет CRUD действия с базой данных
+
+class DataBaseHandler(ABC):
+    '''Базовый класс для работы с базой данных.
     Вызывается через call_method(app, method)'''
-    @staticmethod
-    def call_method(app, method):
+    @classmethod
+    def call_method(cls, app, method):
         '''Проверяет создан ли экземпляр и выполняет метод'''
-        instance = CRUDcompleter(app)
+        instance = cls(app)
         if instance:
-            instance.methods[method]()
-    
+            instance._methods[method]()
+            return instance
+        
     
     def __new__(cls, app):
         '''Проверяет наличие атрибута patient у главного приложения
         и только, если есть атрибут создает экземпляр класса'''
-        # Создает экземпляр
-        instance = super().__new__(cls)
-        
-        # Проверяет наличие атрибута patient у главного приложения
-        # Если нет возвращает None
-        if not hasattr(app, 'patient'):
-            return None
-        
-        return instance
-
+        if hasattr(app, 'patient'): return super().__new__(cls)
+        return None
+    
     
     def __init__(self, app):
-        # Словарь методов
-        self.methods = {
+        # Доступ к экземпляру приложения
+        self._app = app
+        # Доступ к логгеру
+        self._logger = app.logger
+    
+
+    @property
+    @abstractmethod
+    def _methods(self):
+        '''Обязательно определить словарь с методами для каждого класса'''
+        pass
+    
+
+    @property
+    @abstractmethod
+    def _db_cls(self):
+        '''Обязательно определить класс БД'''
+        pass
+
+
+    @property
+    @abstractmethod
+    def _session(self):
+        '''Обязательно определить сессию'''
+        pass
+
+    
+    def _find_patient(self):
+        '''Поиск пациента в базе'''
+        try:
+            with self._session() as db:
+                patients = db.query(self._db_cls).filter(
+                    self._db_cls.surname == self._app.patient.surname
+                ).filter(
+                    self._db_cls.name == self._app.patient.name
+                ).filter(
+                    self._db_cls.patronymic == self._app.patient.patronymic
+                ).filter(
+                    self._db_cls.birth_date == self._app.patient.birth_date
+                ).all()
+        except Exception as err:
+            self._logger.error(f'Ошибка поиска в БД: {repr(err)}')
+            showerror(
+                title=ERROR_MSG,
+                message='Ошибка поиска в БД. Смотрите лог'
+            )
+            return 'error'
+        else: return patients
+
+    
+    def _check_correct_finding(self, patients):
+        '''Проверяет корректность результатов поиска'''
+        # Ошибка при поиске
+        if patients == 'error':
+            self._clear_patient_data()
+            return
+        # Пациент не найден
+        elif not patients:
+            self._clear_patient_data()
+            self._logger.warning('Пациент не найден')
+            showwarning(title=WARNING_MSG, message='Пациент не найден')
+            return
+        # Найдено более одного пациента
+        elif len(patients) > 1:
+            err_msg = 'Найдено более одного пациента. Проверьте базу данных'
+            self._logger.error(err_msg)
+            self._clear_patient_data()
+            showerror(title=ERROR_MSG, message=err_msg)
+            return
+        # Найден один пациент
+        else: return True
+
+    
+    def _clear_patient_data(self):
+        '''Удаляет переменную пациента с неактуальными поисковыми данными
+        и логирует'''
+        attr_name = self._clear_data()
+        self._logger.info(f'Очищен локальный атрибут пациента: {attr_name}')
+
+
+    @abstractmethod
+    def _clear_data(self):
+        '''Определить какой атрибут удаляется и необходимо ли очишения
+        строки пациента на главном окне'''
+        pass
+
+
+    def _error_log_rollback(self, db, err, msg):
+        '''Сообщает об ошибке и записывает в лог, откатывает базу данных'''
+        db.rollback()
+        self._logger.error(f'{msg}: {repr(err)}')
+        showerror(title=ERROR_MSG, message=f'{msg}. Смотрите лог')
+
+
+from sqlalchemy.orm import sessionmaker
+from tkinter.messagebox import showinfo, askyesno
+
+# Мои модули
+from app_data import WARNING_MSG, INFO_MSG, NO_PATIENT
+from ..models import PatientDH
+from ..log_handler import log_patient_attrs
+from .base import DataBaseHandler
+
+
+class DayHospitalHandlerDB(DataBaseHandler):
+    '''Класс для работы с БД дневного стационара'''
+    @property
+    def _methods(self):
+        '''Используемые методы в классе'''
+        return {
             'create': self._create,
             'load': self._load_patient,
         }
-        
-        # Доступ к экземпляру приложения
-        self.app = app
+    
+    @property
+    def _db_cls(self):
+        '''Класс используемой БД'''
+        return PatientDH
 
-        # Доступ к движку базы данных
-        self.engine = app.engine
+    @property
+    def _session(self):
+        '''Сессия для подключения к БД'''
+        return sessionmaker(autoflush=False, bind=self._app.dh_engine)
+    
 
-        # Доступ к логгеру
-        self.logger = app.logger
-
-        # Доступ к переменной надписи пациента на главном окне
+    def __init__(self, app):
+        super().__init__(app)
+        # Доступ к переменной кнопки и надписи пациента на главном окне
         self.patient_label = app.patient_label
-        
-        # Создание класса сессии
-        self.Session = sessionmaker(autoflush=False, bind=self.engine)
-    
-    
+        self.patient_button = app.patient_button
+
+
     def _create(self):
         '''Добавление нового пациента в базу данных или перезапись'''
         # Существует ли уже пациент в базе данных? (поиск)
@@ -75,104 +178,156 @@ class CRUDcompleter:
             else: return
         # Если не найден, добавление пациента в базу данных
         else: self._add_patient_to_db()
-    
 
-    def _add_patient_to_db(self):
-        '''Добавляет пациента в базу данных'''
-        with self.Session() as db:
-            try:
-                db.add(self.app.patient)
-                db.commit()
-                db.refresh(self.app.patient)
-            except Exception as err:
-                msg = 'Ошибка записи в БД'
-                self._error_log_rollback(db, err, msg)
-            else: showinfo(title=INFO_MSG, message='Пациент успешно добавлен')
     
-
     def _update_patient_db(self, finded_patient):
         '''Обновляет данные пациента в базе данных'''
         # Присваивание id найденного пациента локальной версии
-        self.app.patient.id = finded_patient.id
+        self._app.patient.id = finded_patient.id
         # Обновление данных
-        with self.Session() as db:
+        with self._session() as db:
             try:
-                saved_patient = db.merge(self.app.patient)
+                saved_patient = db.merge(self._app.patient)
                 db.commit()
                 db.refresh(saved_patient)
-                self.app.patient = saved_patient
+                self._app.patient = saved_patient
             except Exception as err:
                 msg = 'Ошибка обновления данных в БД'
                 self._error_log_rollback(db, err, msg)
-            else: showinfo(title=INFO_MSG, message='Данные пациента обновлены')
+            else: 
+                showinfo(title=INFO_MSG, message='Данные пациента обновлены')
+                # Блокировка кнопки пациента
+                self.patient_button.config(state='disabled')
+
     
+    def _add_patient_to_db(self):
+        '''Добавляет пациента в базу данных'''
+        with self._session() as db:
+            try:
+                db.add(self._app.patient)
+                db.commit()
+                db.refresh(self._app.patient)
+            except Exception as err:
+                msg = 'Ошибка записи в БД'
+                self._error_log_rollback(db, err, msg)
+            else: 
+                showinfo(title=INFO_MSG, message='Пациент успешно добавлен')
+                # Блокировка кнопки пациента
+                self.patient_button.config(state='disabled')
 
-    def _error_log_rollback(self, db, err, msg):
-        '''Сообщает об ошибке и записывает в лог, откатывает базу данных'''
-        db.rollback()
-        self.logger.error(f'{msg}: {err}')
-        showerror(title=ERROR_MSG, message=f'{msg}. Смотрите лог')
-
-
-    def _find_patient(self):
-        '''Поиск пациента в базе'''
-        try:
-            with self.Session() as db:
-                patients = db.query(Patient).filter(
-                    Patient.surname == self.app.patient.surname
-                ).filter(
-                    Patient.name == self.app.patient.name
-                ).filter(
-                    Patient.patronymic == self.app.patient.patronymic
-                ).filter(
-                    Patient.birth_date == self.app.patient.birth_date
-                ).all()
-        except Exception as err:
-            self.logger.error(f'Ошибка поиска в БД: {err}')
-            showerror(
-                title=ERROR_MSG,
-                message='Ошибка поиска в БД. Смотрите лог'
-            )
-            return 'error'
-        else: return patients
     
-
     def _load_patient(self):
         '''Присвоение глобальной переменной атрибутов найденного пациента'''
+        # Поиск и проверка корректности результатов
         patients = self._find_patient()
-        # Возврат из функции 
-        if patients == 'error':
-            self._clear_search_data()
-        elif not patients:
-            self._clear_search_data()
-            showinfo(title=INFO_MSG, message='Пациент не найден')
-        elif len(patients) > 1:
-            err_msg = 'Найдено более одного пациента. Проверьте базу данных'
-            self.logger.error(err_msg)
-            self._clear_search_data()
-            showerror(title=ERROR_MSG, message=err_msg)
-        else:
+        if self._check_correct_finding(patients):
             # Присвоение найденного пациента глобальной переменной пациента
-            self.app.patient = patients[0]
+            self._app.patient = patients[0]
 
             # Смена текста надписи пациента на главном экране на текущего
             text = (
-                f'{self.app.patient.surname} {self.app.patient.name} '
-                f'{self.app.patient.patronymic} '
-                f'{self.app.patient.birth_date.strftime('%d.%m.%Y')}'
+                f'{self._app.patient.surname} {self._app.patient.name} '
+                f'{self._app.patient.patronymic} '
+                f'{self._app.patient.birth_date.strftime("%d.%m.%Y")}'
             )
             self.patient_label.set(text)
 
             # Внесение в лог
-            self.logger.info('Загружен пациент из базы данных.')
+            self._logger.info('Загружен пациент из базы данных.')
             log_patient_attrs(
-                self.app.patient,
-                self.logger,
-                self.app.patient.all_attrs
+                self._app.patient,
+                self._logger,
+                self._app.patient.all_attrs
             )
+
+    
+    def _clear_data(self):
+        '''Очищает атрибут и строку пациента на главном окне'''
+        self.patient_label.set(NO_PATIENT)
+        delattr(self._app, 'patient')
+        return 'patient'
+    
+from sqlalchemy.orm import sessionmaker
+from tkinter.messagebox import showinfo
+
+# Мои модули
+from app_data import INFO_MSG
+from .base import DataBaseHandler
+from ..models import PatientWL
+
+
+class WaitingListHandlerDB(DataBaseHandler):
+    '''Класс для работы с БД листа ожидания'''
+    @property
+    def _methods(self):
+        '''Используемые методы в классе'''
+        return {
+            'update': self._update_patient,
+            'load': self._load_patient,
+        }
+
+    @property
+    def _db_cls(self):
+        '''Класс используемой БД'''
+        return PatientWL
+
+    @property
+    def _session(self):
+        '''Сессия для подключения к БД'''
+        return sessionmaker(autoflush=False, bind=self._app.wl_engine)
     
 
-    def _clear_search_data(self):
-        '''Удаляет переменную пациента с неактуальными поисковыми данными'''
-        self.patient_label.set(NO_PATIENT)
-        delattr(self.app, 'patient')
+    def __init__(self, app):
+        super().__init__(app)
+        # Флаг при успешном поиске пациента
+        self.success_finding = False
+    
+
+    def _clear_data(self):
+        '''Очищает атрибут пациента'''
+        delattr(self._app, 'patient_wl')
+        return 'patient_wl'
+    
+
+    def _load_patient(self):
+        '''Присвоение глобальной переменной атрибутов найденного пациента'''
+        # Поиск и проверка корректности результатов
+        patients = self._find_patient()
+        if self._check_correct_finding(patients):
+            # Присвоение найденного пациента в переменную ЛС
+            self._app.patient_wl = patients[0]
+            self.success_finding = True
+
+    
+    def _update_patient(self):
+        '''Обновляет данные пациента в Листе ожидания'''
+        with self._session() as db:
+            try:
+                result = db.query(self._db_cls).filter_by(
+                    id=self._app.patient_wl.id
+                ).update(
+                    {
+                        'is_treating': True,
+                        'budget': 'ОМС',
+                        'scheme': self._app.patient.treatment,
+                    }
+                )
+                db.commit()
+                # Если пациент по id не найден (обновлено 0 строк) 
+                # выбрасывается исключение
+                if not result: raise ValueError('ID не найден')
+            except Exception as err:
+                self._clear_patient_data()
+                msg = 'Ошибка обновления данных в Листе ожидания'
+                self._error_log_rollback(db, err, msg)
+            else:
+                self._logger.info(
+                    f'Данные пациента (ID: {self._app.patient_wl.id}) успешно '
+                    'обновлены в Листе ожидания. Статус: взят на лечение, '
+                    'бюджет - ОМС, схема лечения - '
+                    f'{self._app.patient.treatment}'
+                )
+                self._clear_patient_data()
+                showinfo(
+                    title=INFO_MSG, message='Лист ожидания успешно обновлен'
+                )
